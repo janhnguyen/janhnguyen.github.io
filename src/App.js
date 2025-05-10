15ca49639bf0a6cb9d9a83579055165d5f17842a
janhnguyen <nguyenjanh01@gmail.com>
2025-05-10 17:21:51 -0400

    Implemented Header Functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Contact from './components/Contact';
+import Home from './components/Home';
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   return (
+
     <div className="App">
+
       <Header />
-      <header className="App-header">
-        Hello World
-      </header>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+
     </div>
+
   );
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styling/Contact.css';
+
+function Contact() {
+    return (
+        <div>
+            Contact
+        </div>
+    );
+}
+
+export default Contact;
