10222de511d2822629b33dcb01c024e3c4a3e115
janhnguyen <nguyenjanh01@gmail.com>
2025-05-10 17:57:15 -0400

    Added Links and Images to Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import '../styling/Contact.css';
+import emailIcon from '../assets/email.png';
+import githubIcon from '../assets/github.png';
+import linkedinIcon from '../assets/linkedin.png';
+import twitchIcon from '../assets/twitch.png';
 
 function Contact() {
     return (
-        <div>
-            Contact
+        <div className='contact'>
+            Contact Information & Links
+            <br/>
+            <br/>
+            <div className='contact-links'>
+                <a href="mailto:nguyenjanh01@gmail.com">
+                    <img
+                        className="contact-icons"
+                        src={emailIcon}
+                        alt={'Email'}
+                    /></a>
+                <a href="https://github.com/janhnguyen">
+                    <img
+                        className="contact-icons"
+                        src={githubIcon}
+                        alt={'GitHub'}
+                    /></a>
+                <a href="https://www.linkedin.com/in/jason-nguyen-360b7b275/">
+                    <img
+                        className="contact-icons"
+                        src={linkedinIcon}
+                        alt={'LinkedIn'}
+                    /></a>
+                <a href="https://www.twitch.tv/doranis_">
+                    <img
+                        className="contact-icons"
+                        src={twitchIcon}
+                        alt={'Twitch'}
+                    /></a>
+            </div>
         </div>
     );
 }
