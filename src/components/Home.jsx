dd6eb95a5c83b096e62a3ccb18326d95ed51ba74
janhnguyen <nguyenjanh01@gmail.com>
2025-05-10 21:30:18 -0400

    Initial Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,13 @@ import '../styling/Home.css';
 function Home() {
     return (
         <div>
-            Home
+            <div className="twitch-embed">
+                <iframe
+                    src="https://player.twitch.tv/?channel=johnpal&parent=localhost" //replace parent with domain
+                    height="480"
+                    width="800"
+                ></iframe>
+            </div>
         </div>
     );
 }
