7b9483245d99644694eee40c1e50cb1706e8ed57
janhnguyen <nguyenjanh01@gmail.com>
2025-05-10 21:41:54 -0400

    Adjusting for GitHub Pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
         <div>
             <div className="twitch-embed">
                 <iframe
-                    src="https://player.twitch.tv/?channel=johnpal&parent=localhost" //replace parent with domain
+                    src="https://player.twitch.tv/?channel=doranis_&parent=janhnguyen.github.io" //replace parent with domain
                     height="480"
                     width="800"
                 ></iframe>
