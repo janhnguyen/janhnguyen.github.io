a58f0b3f3cc8d84fb5e78db2febea015fdf03412
janhnguyen <nguyenjanh01@gmail.com>
2025-05-11 02:52:38 -0400

    Changed Embed Link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,9 @@ function Home() {
     return (
         <div>
             <div className="twitch-embed">
+                {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
                 <iframe
-                    src="https://player.twitch.tv/?channel=doranis_&parent=janhnguyen.github.io" //replace parent with domain
+                    src="https://player.twitch.tv/?channel=doranis_&parent=janhnguyen-github-io.vercel.app" //replace parent with domain
                     height="480"
                     width="800"
                 ></iframe>
